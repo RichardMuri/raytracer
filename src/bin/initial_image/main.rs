da0c9b89d90885325f3a04166e4ea297643ae4b9
RichardMuri <rmuri@umassd.edu>
2022-07-11 02:38:24 -0400

    Initial check in

---
+++ src/bin/initial_image/main.rs
@@ -0,0 +1,19 @@
+use vec3::*;
+
+fn main() {
+    const IMAGE_WIDTH: i64 = 256;
+    const IMAGE_HEIGHT: i64 = 256;
+
+    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\r Scanlines remaining: {j}");
+        for i in 0..IMAGE_WIDTH {
+            let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+            let pixel: Color<f64> = Color { x: r, y: g, z: b };
+            Vec3::write_color(pixel);
+        }
+    }
+}
---
+++ src/lib/vec3/lib.rs
@@ -0,0 +1,449 @@
+#[macro_use]
+extern crate impl_ops;
+use std::ops;
+
+use num_traits::{cast::FromPrimitive, float::Float};
+use std::fmt;
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Vec3<T: Float> {
+    pub x: T,
+    pub y: T,
+    pub z: T,
+}
+
+// type Point3<T> = Vec3<T>;
+pub type Color<T> = Vec3<T>;
+
+pub fn example<T: Float + FromPrimitive>() -> Vec3<T> {
+    Vec3::zero()
+}
+
+impl<T> Vec3<T>
+where
+    T: Float + FromPrimitive,
+{
+    pub fn zero() -> Vec3<T> {
+        Vec3 {
+            x: T::from_f64(0.0).unwrap(),
+            y: T::from_f64(0.0).unwrap(),
+            z: T::from_f64(0.0).unwrap(),
+        }
+    }
+
+    pub fn one() -> Vec3<T> {
+        Vec3 {
+            x: T::from_f64(1.0).unwrap(),
+            y: T::from_f64(1.0).unwrap(),
+            z: T::from_f64(1.0).unwrap(),
+        }
+    }
+
+    pub fn from_float(value: T) -> Vec3<T> {
+        Vec3 {
+            x: value,
+            y: value,
+            z: value,
+        }
+    }
+
+    fn length_squared(self) -> T {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(self) -> T {
+        let result = self.length_squared();
+        result.sqrt()
+    }
+
+    #[inline]
+    pub fn dot(u: &Vec3<T>, v: &Vec3<T>) -> T {
+        u[0] * v[0] + u[1] * v[1] + u[2] * v[2]
+    }
+
+    #[inline]
+    pub fn cross(u: &Vec<T>, v: &Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: u[1] * v[2] - u[2] * v[1],
+            y: u[2] * v[0] - u[0] * v[2],
+            z: u[0] * v[1] - u[1] * v[0],
+        }
+    }
+
+    #[inline]
+    pub fn unit_vector(v: Vec3<T>) -> Vec3<T> {
+        return v / v.length();
+    }
+
+    pub fn write_color(self) {
+        // Write the translated [0,255] value of each color component.
+        let ir: i32 = (255.999 * T::to_f64(&self.x).unwrap()) as i32;
+        let ig: i32 = (255.999 * T::to_f64(&self.y).unwrap()) as i32;
+        let ib: i32 = (255.999 * T::to_f64(&self.z).unwrap()) as i32;
+
+        println!("{ir} {ig} {ib}");
+    }
+}
+
+impl Vec3<f32> {
+    pub const ZERO: Vec3<f32> = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    pub const ONE: Vec3<f32> = Vec3 {
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+    };
+}
+
+impl Vec3<f64> {
+    pub const ZERO: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    pub const ONE: Vec3<f64> = Vec3 {
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+    };
+}
+
+impl<T: Float + fmt::Display> fmt::Display for Vec3<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+impl<T: Float> Index<usize> for Vec3<T> {
+    type Output = T;
+    #[inline]
+    fn index(&self, i: usize) -> &Self::Output {
+        match i {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!(
+                "Vec3 index must be 0, 1, or 2 corresponding to x, y, or z, got {}.",
+                i,
+            ),
+        }
+    }
+}
+
+impl<T: Float> IndexMut<usize> for Vec3<T> {
+    #[inline]
+    fn index_mut(&mut self, i: usize) -> &mut T {
+        match i {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => panic!(
+                "Vec3 index must be 0, 1, or 2 corresponding to x, y, or z, got {}.",
+                i,
+            ),
+        }
+    }
+}
+
+// impl<T: Float> Add for Vec3<T> {
+//     type Output = Vec3<T>;
+
+//     #[inline]
+//     fn add(self, other: Vec3<T>) -> Vec3<T> {
+//         Vec3 {
+//             x: self.x + other.x,
+//             y: self.y + other.y,
+//             z: self.z + other.z,
+//         }
+//     }
+// }
+
+// impl<T: Float> Add<T> for Vec3<T> {
+//     type Output = Vec3<T>;
+
+//     #[inline]
+//     fn add(self, other: T) -> Vec3<T> {
+//         Vec3 {
+//             x: self.x + other,
+//             y: self.y + other,
+//             z: self.z + other,
+//         }
+//     }
+// }
+// struct MyType<T: Float>(T);
+// impl<T: Float> Add<Vec3<T>> for MyType<T> {
+//     type Output = Vec3<T>;
+//     #[inline]
+//     fn add(self, other: Vec3<T>) -> Vec3<T> {
+//         other + self.0
+//     }
+// }
+
+// impl<T: Float> AddAssign for Vec3<T> {
+//     #[inline]
+//     fn add_assign(&mut self, other: Vec3<T>) {
+//         *self = *self + other;
+//     }
+// }
+
+// impl<T: Float> AddAssign<T> for Vec3<T> {
+//     #[inline]
+//     fn add_assign(&mut self, other: T) {
+//         *self = *self + other;
+//     }
+// }
+
+impl_op_ex!(+ |a: &Vec3<f32>, b: &Vec3<f32>| -> Vec3<f32> {Vec3{x: a.x + b.x, y: a.y + b.y, z: a.z + b.z}});
+impl_op_ex!(+ |a: &Vec3<f64>, b: &Vec3<f64>| -> Vec3<f64> {Vec3{x: a.x + b.x, y: a.y + b.y, z: a.z + b.z}});
+impl_op_ex_commutative!(+ |a: &Vec3<f32>, b: f32| -> Vec3<f32> { Vec3{x: a.x + b, y: a.y + b, z: a.z + b}});
+impl_op_ex_commutative!(+ |a: &Vec3<f64>, b: f64| -> Vec3<f64> { Vec3{x: a.x + b, y: a.y + b, z: a.z + b}});
+impl_op_ex!(+= |a: &mut Vec3<f32>, b: Vec3<f32>| { *a = *a + b});
+impl_op_ex!(+= |a: &mut Vec3<f64>, b: Vec3<f64>| { *a = *a + b });
+impl_op_ex!(+= |a: &mut Vec3<f32>, b: f32| { *a = *a + b });
+impl_op_ex!(+= |a: &mut Vec3<f64>, b: f64| { *a = *a + b });
+
+impl<T: Float> Sub for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn sub(self, other: Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl<T: Float> Sub<T> for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn sub(self, other: T) -> Vec3<T> {
+        Vec3 {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        }
+    }
+}
+
+impl<T: Float> SubAssign for Vec3<T> {
+    #[inline]
+    fn sub_assign(&mut self, other: Vec3<T>) {
+        *self = *self - other;
+    }
+}
+
+impl<T: Float> SubAssign<T> for Vec3<T> {
+    #[inline]
+    fn sub_assign(&mut self, other: T) {
+        *self = *self - other;
+    }
+}
+
+impl<T: Float> Mul for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn mul(self, other: Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl<T: Float> Mul<T> for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn mul(self, other: T) -> Vec3<T> {
+        Vec3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl<T: Float> MulAssign for Vec3<T> {
+    #[inline]
+    fn mul_assign(&mut self, other: Vec3<T>) {
+        *self = *self * other;
+    }
+}
+
+impl<T: Float> MulAssign<T> for Vec3<T> {
+    #[inline]
+    fn mul_assign(&mut self, other: T) {
+        *self = *self * other;
+    }
+}
+
+impl<T: Float> Div for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn div(self, other: Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl<T: Float> Div<T> for Vec3<T> {
+    type Output = Vec3<T>;
+
+    #[inline]
+    fn div(self, other: T) -> Vec3<T> {
+        Vec3 {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+impl<T: Float> DivAssign for Vec3<T> {
+    #[inline]
+    fn div_assign(&mut self, other: Vec3<T>) {
+        *self = *self / other;
+    }
+}
+
+impl<T: Float> DivAssign<T> for Vec3<T> {
+    #[inline]
+    fn div_assign(&mut self, other: T) {
+        *self = *self / other;
+    }
+}
+
+impl<T: Float> Neg for Vec3<T> {
+    type Output = Self;
+
+    #[inline]
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+#[test]
+fn indexing() {
+    let x: Vec3<f64> = Vec3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    assert_eq!(x[0], 1.0);
+    assert_eq!(x[1], 2.0);
+
+    let mut y: Vec3<f64> = Vec3 {
+        x: 10.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    y[0] = 1.0;
+    assert_eq!(y, x);
+}
+
+#[test]
+#[should_panic(expected = "Vec3 index must be 0, 1, or 2 corresponding to x, y, or z, got 5.")]
+fn invalid_indexing() {
+    let x: Vec3<f64> = Vec3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    x[5];
+}
+
+#[test]
+fn addition() {
+    let x: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
+    };
+    let y: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
+    };
+    let z: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 2.0,
+        z: 4.0,
+    };
+    let x_plus_one: Vec3<f64> = Vec3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    // Test Vec3 + Vec3
+    assert_eq!(z, x + y);
+    // Test Vec3 + float
+    assert_eq!(x_plus_one, x + 1.0);
+    // Test float + Vec3
+    assert_eq!(x_plus_one, 1.0 + x);
+
+    let mut mx: Vec3<f64> = x;
+    let mut my: Vec3<f64> = y;
+
+    // Test += operator
+    mx += my;
+    assert_eq!(mx, z);
+
+    // Ensure commutativity still works
+    mx = x;
+    my += mx;
+    assert_eq!(my, z);
+}
+
+#[test]
+fn subtraction() {
+    let x: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
+    };
+    let y: Vec3<f64> = Vec3 {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
+    };
+    let x_minus_one: Vec3<f64> = Vec3 {
+        x: -1.0,
+        y: 0.0,
+        z: 1.0,
+    };
+    assert_eq!(Vec3::<f64>::ZERO, x - y);
+    assert_eq!(x_minus_one, x - 1.0);
+}
+
+#[test]
+fn length() {
+    let x: Vec3<f64> = Vec3 {
+        x: 2.0,
+        y: 3.0,
+        z: 6.0,
+    };
+    let y = 7.0;
+    assert_eq!(x.length(), y);
+    assert_eq!(Vec3::length(x), y);
+}
